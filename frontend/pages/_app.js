f8be02d936e78d3e693ac16f32c3c05280695906
amberlywalter <amberlygwalter@gmail.com>
2025-10-30 22:32:28 -0400

    Update code

--- frontend/pages/_app.js
+++ frontend/pages/_app.js
@@ -1,5 +1,5 @@
 // frontend/pages/_app.js
-import "@/styles/globals.css";
+import "../styles/global.css";
 
 export default function App({ Component, pageProps }) {
   return <Component {...pageProps} />;
